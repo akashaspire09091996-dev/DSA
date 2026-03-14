2bfdea675b1cc088de8f21311ac8b1ab8518610c
akashaspire09091996-dev <akashaspire09091996@gmail.com>
2026-03-14 20:46:29 +0530

    update

--- Akash/index.js
+++ Akash/index.js
@@ -1,1 +1,60 @@
 
+//Insert a char
+
+let s = "Geeks",
+    let c = 'A',
+        let pos = 3
+let subStrings = s.split(3)
+let res = subStrings[0] + c + subStrings[1]
+
+let new = ""
+for (let i = 0; i < s.length; i++) {
+    if (i == pos) {
+        new += c
+    } else {
+        new += s[i]
+    }
+}
+
+
+// Remove a char
+Input : s = "abcde",  pos = 1
+Output : s = "acde"
+let s = "abcde";
+let pos = 1;
+let res = "";
+for( let i =0; i < s.length; i++ ) {
+	if( i == pos ) continue
+	res += s[i]
+}
+console.log(res)
+
+
+let s = "abcdcdd"
+let c = "d"
+let res = ""
+function remove( i ) { 
+ if( s[i] !== c ) res += s[i]
+ if( s[i] === undefined ) return res;
+ return remove( i+1 )
+}
+
+remove(0)
+
+let s = "abcdcdd"
+s[0] =>>> a , s[6] ===> d
+
+[a, d] = [d, a];
+
+//Camel case of a given sentence
+
+let res
+
+
+
+
+
+
+
+
+
